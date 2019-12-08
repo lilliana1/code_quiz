3c8a6ab23e22c322aa3ed36d3ea8c4813e121c16
Lilliana Ramos <lilliana.cramos@gmail.com>
2019-12-08 09:49:31 -0500

    updated readme & comments

--- README.md
+++ README.md
@@ -1,61 +1,25 @@
-# code_quiz
+#  Code Quiz
+![picture](./assets/images/code_quiz.png)
+## Introduction
+This Code Quiz will test you on your JS skills but, you must do it fast because time is running. Everytime you get a wrong answer, you will get penalize. Good Luck!
 
-## pseudocode
-1. start with a landing page
-    * link to view highscores
-    * timer
-    * start button to start game
-    * game description/rule
+## Code 
 
-2. when user click the start button
-    * descitpion text and button to dissappear
-    * questions to display
-    * 4 options/choices
-    * start time have count
 
-3. when user select an option/choice button
-    * evaluate the choice
-        * if user gets it right
-            * increment the correct counter
-        * if wrong
-            * reduce time
+## Technologies
+* HTML
+* CSS
+* JS
 
-4. the timer stop when all questions have been answer or the timer runs out
-    * game is over
-    * reveal final correct & highscore
-        * allow user to enter initials to be added to highscore table
+## Instalation
+1. On GitHub, navigate to the main page of the repository.
+2. Under the repository name, click Clone or download.
+3. To clone the repository, click "Clone with HTTPS".
+4. Open Terminal.
+5. Change the current working directory to the location where you want the cloned directory to be made.
+6. Type git clone, and then paste the URL you copied in Step 2.
+7. Press Enter. Your local clone will be created.
 
-5. when user is looking at highscore table
-    * reroute back to the game ej de latitud 
-
-*** notes ***
-1. build an array w/ question
-2. create a function called startQuiz()
-    * hie landing/start page
-    * reveal the question
-    * set timer
-    * run questionFunction
-
-3. timer function 
-4. function called showQuestion 
-    * show the questions and answers (dynamically )
-5. function cycleFunction 
-    * cycle through questions array (hide previous question and shows next)
-    * validate right or wrong
-        * if wrong 
-            * reduce time
-            * play wrong sound
-                * goes to next questions
-        * else
-            * play right sound
-            * goes to next question 
-            * modal to show correct
-    * if no more questions
-        * hide result    
-        * go to the result screen
-6. function endQuiz 
-    * this will show results/final score
-        * final score will be whatever the remaining time is 
-7. function to save highscore
-
+## Authors
+* Lilliana Ramos
 
--- quiz.js
+++ quiz.js
@@ -1,9 +1,7 @@
-  
 // select all elements
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
-// const qImg = document.getElementById("qImg");
 const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
@@ -13,20 +11,17 @@ const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
 
-
-
-// create our questions
+// questions to be asked
 let questions = [
-    //original question #1
-    // choices dont appear 
-    // {
-    //     question: "Inside which HTML element do we put the JavaScript?",
-    //     choiceA: "<scripting>",
-    //     choiceB: "<javascript>",
-    //     choiceC: "<script>",
-    //     correct: "C"
-    // },
-     // new question #1
+    //question #1
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choiceA: "< scripting >",
+        choiceB: "< javascript >",
+        choiceC: "< script >",
+        correct: "C"
+    },
+     //question #2
     {
         question: "Where is the correct place to insert a JavaScript?",
         choiceA: "Both the <head> section and the <body> section are correct",
@@ -34,7 +29,7 @@ let questions = [
         choiceC: "The < body > section",
         correct: "A"
     },
-    //question #2
+    //question #3
     {
         question: "The external JavaScript file must contain the < script > tag.",
         choiceA: "True",
@@ -42,7 +37,7 @@ let questions = [
         choiceC: "🤔",
         correct: "B"
     },
-    //question #3
+    //question #4
     {
         question: "How do you write a function in JavaScript?",
         choiceA: "function: myFunction()",
@@ -50,7 +45,7 @@ let questions = [
         choiceC: "function = myFuction()",
         correct: "B"
     },
-    //question #4
+    //question #5
     {
         question: "How do you call a function named myFuction?",
         choiceA: "call myFunction()",
@@ -58,7 +53,7 @@ let questions = [
         choiceC: "myFuction()",
         correct: "C"
     },
-    //question #5
+    //question #6
     {
         question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
         choiceA: "if (i != 5)",
@@ -68,27 +63,25 @@ let questions = [
     }
 ];
 
-// create some variables
-
+// global variables
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
-let count = 75;
-const questionTime = 75; // 10s
-const gaugeWidth = 150; // 150px
+let count = 76;
+const questionTime = 75;
+const gaugeWidth = 150;
 const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
 let score = 0;
 
-// render a question
+// ask a question
 function renderQuestion(){
     let q = questions[runningQuestion];
-    
     question.innerHTML = "<p>"+ q.question +"</p>";
-    // qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    // user has 3 choices to pick from
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
-    // choiceD.innerHTML = q.choiceD;
+    
 }
 
 start.addEventListener("click",startQuiz);
@@ -102,7 +95,7 @@ function startQuiz(){
     quiz.style.display = "block";
     renderProgress();
     renderCounter();
-    TIMER = setInterval(renderCounter,1000); // 1000ms = 1s
+    TIMER = setInterval(renderCounter,1000); 
    
 }
 
@@ -114,45 +107,15 @@ function renderProgress(){
 }
 
 // counter render
-
 function renderCounter(){
     count--;
     counter.innerHTML = count;
-
     if (count <= 0) {
         scoreRender()
-    }
-    
-    // if(answerIsCorrect){
-    //     console.log("correct");
-        
-        
-    //     timeGauge.style.width = count * gaugeUnit + "px";
-        
-        
-    //     if (count === 0){
-    //         console.log("aja");
-            
-    //     }
-    // }else{
-        
-    //     // change progress color to red
-    //     answerIsWrong();
-    //     if(runningQuestion < lastQuestion){
-    //         runningQuestion++;
-    //         renderQuestion();
-    //     }else{
-    //         // end the quiz and show the score
-    //         clearInterval(TIMER);
-    //         scoreRender();
-    //     }
-    // }
+    }    
 }
 
-
-
-// checkAnwer
-
+// checking if answer is correct 
 function checkAnswer(answer){
     if( answer == questions[runningQuestion].correct){
         // answer is correct
@@ -162,36 +125,35 @@ function checkAnswer(answer){
         // change progress color to green
         // answerIsCorrect();
     }else {
+        // -15 seconds when answer is incorrect
         count = count - 15;
-        
         // alert("INCORRECT!🤬");
         // answer is wrong
         // change progress color to red
         // answerIsWrong();
     }
 
-    if(runningQuestion < lastQuestion){
+    if(runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
-    }else {
+    } else {
         // end the quiz and show the score
         clearInterval(TIMER);
         scoreRender();
     }
 }
 
-// answer is correct
+// answer is correct lets continue questions
 function answerIsCorrect(){
     document.getElementById(runningQuestion);
     
     
 }
 
-// answer is Wrong
+// answer is wrong lets continue questions
 function answerIsWrong(){
     document.getElementById(runningQuestion);
 }
-
 
 // score render
 function scoreRender(){
@@ -204,16 +166,17 @@ function scoreRender(){
         "highscore": score,
         "initials": initials
     }
-    
+
+    // store highscore and initials
     localStorage.setItem("highscore", JSON.stringify(highscore));
   
     scoreDiv.style.display = "block";
     
     // calculate the amount of question percent answered by the user
-    const scorePerCent = Math.round(100 * score/questions.length);
+    // const scorePerCent = Math.round(100 * score/questions.length);
     
-    //scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePerCent +"</p>";
+    // display score 
+    // scoreDiv.innerHTML += "<p>"+ scorePerCent +"</p>";
     scoreDiv.innerHTML += "<p>" + score + "</p>";
     scoreDiv.innerHTML += "<p>" + initials + "</p>";
 
